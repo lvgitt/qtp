4c9ccea1733ec8e94eaa80d5e07614a74fa3bb90
lvwf <lvwf@homeking365.com>
2022-09-01 09:54:20 +0800

    字段JSON转换

---
+++ test-case/src/main/java/cn/test/dto/Order.java
@@ -0,0 +1,32 @@
+package cn.test.dto;
+
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author lwf
+ * @since 2022-08-18 18:45:27
+ */
+@Data
+@SuppressWarnings("serial")
+//@TableName(autoResultMap = true,value = "order")
+public class Order  {
+    /**
+     * ID主键
+     */
+    private Long id;
+    /**
+     * 用户ID
+     */
+    private Long userId;
+    /**
+     * 关联ID列表 JSON类型：[111,222,333]
+     */
+//    @TableField(typeHandler = JsonTypeHandler.class)
+    private List<Number> boxIdList;
+}
+
